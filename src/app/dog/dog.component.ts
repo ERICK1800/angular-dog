dea9e97a8b0e45553e037bae102bf5a0a5a8e69d
ERICK1800 <ericklimaflorencio@gmail.com>
2023-04-18 11:31:55 -0300

    cachorrinhos aleatórios

--- src/app/dog/dog.component.ts
+++ src/app/dog/dog.component.ts
@@ -1,35 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Dog } from '../dog';
+import { DogService } from '../dog.service';
 
 @Component({
   selector: 'app-dog',
   templateUrl: './dog.component.html',
   styleUrls: ['./dog.component.css']
 })
-export class DogComponent {
-  dog1 : Dog = {
-    "message" : "https://images.dog.ceo/breeds/spaniel-cocker/n02102318_10355.jpg",
-    "status" : "true"
-  } as Dog;
+export class DogComponent implements OnInit{
 
+  constructor(private dogService: DogService) {}
+  ngOnInit(): void {
+    this.loadDog1();
+    this.loadDog2();
+    this.loadDog3();
+  }
+
+  loadDog1() {
+    this.dogService.getRandomDog().subscribe(
+      {next : dog => this.dog1 = dog}
+    );
+  }
+  dog1 : Dog = {} as Dog;
   getName1(): string []{
     return this.dog1.message.split("/")[4].split("-");
   }
 
-  dog2 : Dog = {
-    "message" : "https://images.dog.ceo/breeds/terrier-welsh/lucy.jpg",
-    "status" : "true"
-  } as Dog;
-
+  loadDog2() {
+    this.dogService.getRandomDog().subscribe(
+      {next : dog => this.dog2 = dog}
+    );
+  }
+  dog2 : Dog = {} as Dog;
   getName2(): string []{
     return this.dog2.message.split("/")[4].split("-");
   }
 
-  dog3 : Dog = {
-    "message" : "https://images.dog.ceo/breeds/mountain-swiss/n02107574_1033.jpg",
-    "status" : "true"
-  } as Dog;
-
+  loadDog3() {
+    this.dogService.getRandomDog().subscribe(
+      {next : dog => this.dog3 = dog}
+    );
+  }
+  dog3 : Dog = {} as Dog;
   getName3(): string []{
     return this.dog3.message.split("/")[4].split("-");
   }
