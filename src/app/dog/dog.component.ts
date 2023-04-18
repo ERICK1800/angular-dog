f46e1f271505cdd0d3d01b04adf1e690d5356166
ERICK1800 <ericklimaflorencio@gmail.com>
2023-04-18 10:49:13 -0300

    muitos dogs

--- src/app/dog/dog.component.ts
+++ src/app/dog/dog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Dog } from '../dog';
 
 @Component({
   selector: 'app-dog',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./dog.component.css']
 })
 export class DogComponent {
+  dog1 : Dog = {
+    "message" : "https://images.dog.ceo/breeds/spaniel-cocker/n02102318_10355.jpg",
+    "status" : "true"
+  } as Dog;
 
+  getName1(): string []{
+    return this.dog1.message.split("/")[4].split("-");
+  }
+
+  dog2 : Dog = {
+    "message" : "https://images.dog.ceo/breeds/terrier-welsh/lucy.jpg",
+    "status" : "true"
+  } as Dog;
+
+  getName2(): string []{
+    return this.dog2.message.split("/")[4].split("-");
+  }
+
+  dog3 : Dog = {
+    "message" : "https://images.dog.ceo/breeds/mountain-swiss/n02107574_1033.jpg",
+    "status" : "true"
+  } as Dog;
+
+  getName3(): string []{
+    return this.dog3.message.split("/")[4].split("-");
+  }
 }
